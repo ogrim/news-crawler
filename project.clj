1bc1135951aafd4e0ee10da63a83a249d95be79b
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-08-30 15:45:54 +0200

    Updated configs

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject news-crawler "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies (
-                 [enlive "1.0.0-SNAPSHOT"]
-                 ;  [enlive "1.0.0"]
-                   [org.clojure/clojure "1.2.1"]
-                   [org.clojure/clojure-contrib "1.2.0"])
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"})
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [enlive "1.0.0"]]
+  :dev-dependencies [[org.clojars.weavejester/autodoc "0.9.0"]])
