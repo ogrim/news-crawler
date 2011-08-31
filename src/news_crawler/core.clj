6879f08b0c1eb559ae44f6abb79eb3d20f5574ab
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-08-31 15:56:30 +0200

    Worked Added stuff to core.clj

--- src/news_crawler/core.clj
+++ src/news_crawler/core.clj
@@ -1,1 +1,16 @@
-(ns news-crawler.core)
+(ns news-crawler.core
+  (:require [news-crawler.downloader :as d]))
+
+(def *url-data* [["bt" "http://bt.no"]
+                 ["ba" "http://ba.no"]
+                 ])
+
+; (d/download-all *url-data* 2 "fetched/")
+
+
+
+
+
+
+
+
