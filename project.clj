bed00a7b6cf3f911fff0713549a5ff4ba8dd349a
Aleksander Skjæveland Larsen <ogrim@localhost.localdomain>
2011-08-31 09:50:47 +0200

    Set version to 0.1.0 and added description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject news-crawler "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject news-crawler "0.1.0"
+  :description "A library for downloading large number of URLs, in addition to parse HTML from online newspapers"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [enlive "1.0.0"]]
