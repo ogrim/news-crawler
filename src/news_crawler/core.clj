f57b4c4dbd4b42a5fc1dbc4d9561f143129f27d7
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-03 01:25:23 +0200

    Added function to autogenerate filenames for lists of urls, for the downloader

--- src/news_crawler/core.clj
+++ src/news_crawler/core.clj
@@ -5,10 +5,12 @@
                  ["ba" "http://ba.no"]
                  ])
 
+(defn urls->vector [urls]
+  (let [i (ref 0)]
+    (map #(vector (dosync (alter i inc)) %) urls)))
+
 ; (d/download-all *url-data* 2 "fetched/")
-
-
-
+; (d/download-all bt-urls-vectors 2 "fetched/")
 
 
 
