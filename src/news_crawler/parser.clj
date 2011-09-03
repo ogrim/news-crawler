335d19d7612b69465c72892103b993aee7fedc14
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-03 15:50:26 +0200

    Added function to define filters with ease

--- src/news_crawler/parser.clj
+++ src/news_crawler/parser.clj
@@ -8,10 +8,28 @@
     (html-resource r)))
 
 (defn all-links [html-map]
-  (map #(:href (:attrs %)) (select html-map [:a])))
+  (distinct (map #(:href (:attrs %)) (select html-map [:a]))))
 
-(defn link-filter [link]
+(defn validate-link [link]
   (cond (< (count link) 11) false
         (= (subs link 0 4) "http") true
         :else false))
+
+(defn link-filter
+  ([link must-contain] (if (re-seq (re-pattern must-contain) link) true false))
+  ([link must-contain & or-clauses]
+  (let [expanded-clauses (filter #(re-seq (re-pattern %) link) or-clauses)]
+    (if (and (re-seq (re-pattern must-contain) link)
+             (< 0 (count expanded-clauses)))
+      true false))))
+
+
+(comment
+(def links (all-links (file->map "fetched/bt")))
+(def validated-links (filter #(validate-link %) links))
+(def bt-filter #(link-filter % "nyheter" ".html\\b" ".ece\\b"))
+(def filtered-bt (filter bt-filter validated-links))
+(spit "fetched/bt-a" (seq (filtered-bt)))
+)
+
 
