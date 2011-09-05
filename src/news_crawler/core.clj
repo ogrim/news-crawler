d60dc6206295d8497bc0c6de2d765cb63e8b69bd
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-05 13:52:06 +0200

    Added clj-time dependency and updated filenames with date

--- src/news_crawler/core.clj
+++ src/news_crawler/core.clj
@@ -1,10 +1,15 @@
 (ns news-crawler.core
   (:require [news-crawler.downloader :as d])
-  (:use [news-crawler.parser]))
+  (:use [news-crawler.parser])
+  (:use [clj-time.core :only (now year month day)]))
 
 (def *url-data* [["bt" "http://bt.no"]
                  ["ba" "http://ba.no"]
                  ])
+
+(defn current-date []
+  (let [date (now)]
+    (str (year date) "-" (month date) "-" (day date))))
 
 (defn urls->vector
   "Convert a coll of URLs to downloadable format
@@ -12,7 +17,7 @@
    [\"url1\" \"url2\"] -> [[\"bt-1\" \"url1\"] [\"bt-2\" \"url2\"]]"
   [basename urls]
   (let [i (ref 0)]
-    (map #(vector (str basename "-" (dosync (alter i inc))) %) urls)))
+    (map #(vector (str basename "_" (current-date) "_" (dosync (alter i inc))) %) urls)))
 
 (defilter bt-filter "nyheter" ".html\\b" ".ece\\b")
 (defilter ba-filter ".ece\\b" "nyheter" "puls")
@@ -20,7 +25,6 @@
 
 ; (d/download-all *url-data* 2 "fetched/")
 ; (d/download-all bt-urls-vectors 2 "fetched/")
-
 
 
 
