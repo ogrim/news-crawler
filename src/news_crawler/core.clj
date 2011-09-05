fe57bc5a971df6d3050f32db439a861900aab1d1
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-05 11:16:12 +0200

    Defined filters in core

--- src/news_crawler/core.clj
+++ src/news_crawler/core.clj
@@ -1,13 +1,22 @@
 (ns news-crawler.core
-  (:require [news-crawler.downloader :as d]))
+  (:require [news-crawler.downloader :as d])
+  (:use [news-crawler.parser]))
 
 (def *url-data* [["bt" "http://bt.no"]
                  ["ba" "http://ba.no"]
                  ])
 
-(defn urls->vector [urls]
+(defn urls->vector
+  "Convert a coll of URLs to downloadable format
+   Example:
+   [\"url1\" \"url2\"] -> [[\"bt-1\" \"url1\"] [\"bt-2\" \"url2\"]]"
+  [basename urls]
   (let [i (ref 0)]
-    (map #(vector (dosync (alter i inc)) %) urls)))
+    (map #(vector (str basename "-" (dosync (alter i inc))) %) urls)))
+
+(defilter bt-filter "nyheter" ".html\\b" ".ece\\b")
+(defilter ba-filter ".ece\\b" "nyheter" "puls")
+
 
 ; (d/download-all *url-data* 2 "fetched/")
 ; (d/download-all bt-urls-vectors 2 "fetched/")
