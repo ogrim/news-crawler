042418b55294dee8ebf7e3386c9d6c8e74f90b8f
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-05 11:15:39 +0200

    Updated several small things

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject news-crawler "0.2.0"
+(defproject news-crawler "0.3.0"
   :description "A library for downloading large number of URLs, in addition to parse HTML from online newspapers"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
--- src/news_crawler/parser.clj
+++ src/news_crawler/parser.clj
@@ -2,7 +2,6 @@
   (:use [net.cgrand.enlive-html])
   (:use [clojure.contrib.duck-streams :only (reader)]))
 
-
 (defn file->map [file]
   (with-open [r (reader file)]
     (html-resource r)))
@@ -16,12 +15,13 @@
         :else false))
 
 (defn link-filter
-  ([link must-contain] (if (re-seq (re-pattern must-contain) link) true false))
+  ([link must-contain]
+     (if (re-seq (re-pattern must-contain) link) true false))
   ([link must-contain & or-clauses]
-  (let [expanded-clauses (filter #(re-seq (re-pattern %) link) or-clauses)]
-    (if (and (re-seq (re-pattern must-contain) link)
-             (< 0 (count expanded-clauses)))
-      true false))))
+     (let [expanded-clauses (filter #(re-seq (re-pattern %) link) or-clauses)]
+       (if (and (re-seq (re-pattern must-contain) link)
+                (< 0 (count expanded-clauses)))
+         true false))))
 
 (defmacro defilter
   ([name must-contain]
@@ -38,5 +38,4 @@
 (def filtered-bt (filter bt-filter validated-links))
 (spit "fetched/bt-a" (seq (filtered-bt)))
 )
-
 
