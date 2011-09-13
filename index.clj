cf8bc37581726526b097a4fc9b1a56a8adf10f73
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2011-09-13 17:16:36 +0200

    Updated API documentation for 2.0.0

--- index.clj
+++ index.clj
@@ -4,6 +4,10 @@
    :name "news-crawler.core",
    :doc nil}
   {:source-url nil,
+   :wiki-url "news-crawler.db-api.html",
+   :name "news-crawler.db",
+   :doc nil}
+  {:source-url nil,
    :wiki-url "news-crawler.downloader-api.html",
    :name "news-crawler.downloader",
    :doc nil}
@@ -14,10 +18,20 @@
  :vars
  ({:raw-source-url nil,
    :source-url nil,
+   :wiki-url "/news-crawler.core-api.html#news-crawler.core/*db*",
+   :namespace "news-crawler.core",
+   :line 26,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj",
+   :var-type "var",
+   :doc
+   "Defines database settings, which are passed to news-crawler.db",
+   :name "*db*"}
+  {:raw-source-url nil,
+   :source-url nil,
    :wiki-url "/news-crawler.core-api.html#news-crawler.core/*out-dir*",
    :namespace "news-crawler.core",
-   :line 12,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj",
+   :line 13,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj",
    :var-type "var",
    :doc "Output directory, where files get stored",
    :name "*out-dir*"}
@@ -26,24 +40,12 @@
    :wiki-url
    "/news-crawler.core-api.html#news-crawler.core/*url-data*",
    :namespace "news-crawler.core",
-   :line 16,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj",
+   :line 17,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj",
    :var-type "var",
    :doc
-   "Defines the sites to scrape, and what filter and parser functions to use\n[[name url article-selector article-filter article-parser][..]]",
+   "Defines the sites to scrape, and what filter and parser functions to use.\nThe selector matches articles on the front page, while the filter will reduce\nthe selection, the parser will scrape the article content.\n   [[name url article-selector article-filter article-parser][..]]",
    :name "*url-data*"}
-  {:arglists ([urls]),
-   :name "append-date",
-   :namespace "news-crawler.core",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url
-   "/news-crawler.core-api.html#news-crawler.core/append-date",
-   :doc
-   "Appends the date to the name in *url-data*\nUsed to create daily folder, and filenames",
-   :var-type "function",
-   :line 37,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
   {:arglists ([]),
    :name "current-date",
    :namespace "news-crawler.core",
@@ -53,32 +55,9 @@
    "/news-crawler.core-api.html#news-crawler.core/current-date",
    :doc "Current date as yyyy-m-d",
    :var-type "function",
-   :line 23,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
-  {:arglists ([]),
-   :name "daily",
-   :namespace "news-crawler.core",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url "/news-crawler.core-api.html#news-crawler.core/daily",
-   :doc
-   "Call with *url-data* and *out-dir* properly set, \nin order to output the resulting data into maps",
-   :var-type "function",
-   :line 60,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
-  {:arglists ([urls]),
-   :name "filter-daily-links",
-   :namespace "news-crawler.core",
-   :source-url nil,
-   :raw-source-url nil,
-   :wiki-url
-   "/news-crawler.core-api.html#news-crawler.core/filter-daily-links",
-   :doc
-   "Reads in the downloaded front page, and extracts article URLs",
-   :var-type "function",
-   :line 43,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
-  {:arglists ([filenames parser]),
+   :line 33,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj"}
+  {:arglists ([html-list parser urls]),
    :name "parse-articles",
    :namespace "news-crawler.core",
    :source-url nil,
@@ -86,23 +65,45 @@
    :wiki-url
    "/news-crawler.core-api.html#news-crawler.core/parse-articles",
    :doc
-   "Reads in the downloaded articles, and extract the article content",
+   "Takes a list of downloaded articles, urls and the parser.\nExtracts the content of the articles to maps",
    :var-type "function",
-   :line 53,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
-  {:arglists ([basename urls]),
-   :name "urls->vector",
+   :line 46,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj"}
+  {:arglists ([]),
+   :name "scrape",
+   :namespace "news-crawler.core",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url "/news-crawler.core-api.html#news-crawler.core/scrape",
+   :doc
+   "Runs the scraping with configurations defined by *out-dir*, *url-data* and *db*",
+   :var-type "function",
+   :line 54,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj"}
+  {:arglists ([url selector filter-func]),
+   :name "url->links",
    :namespace "news-crawler.core",
    :source-url nil,
    :raw-source-url nil,
    :wiki-url
-   "/news-crawler.core-api.html#news-crawler.core/urls->vector",
+   "/news-crawler.core-api.html#news-crawler.core/url->links",
    :doc
-   "Convert a coll of URLs to downloadable format with basename and current date\nExample:\n[\"url1\" \"url2\"] -> [[\"bt_2011-9-6_1\" \"url1\"] [\"bt_2011-9-6_2\" \"url2\"]]",
+   "Extracts the article links from url using selector and filter-function",
    :var-type "function",
-   :line 29,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/core.clj"}
-  {:arglists ([filename url milliseconds]),
+   :line 39,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/core.clj"}
+  {:arglists ([db articles]),
+   :name "insert-articles",
+   :namespace "news-crawler.db",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "/news-crawler.db-api.html#news-crawler.db/insert-articles",
+   :doc "Inserts all articles into database",
+   :var-type "function",
+   :line 17,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/db.clj"}
+  {:arglists ([url milliseconds]),
    :name "download",
    :namespace "news-crawler.downloader",
    :source-url nil,
@@ -110,12 +111,12 @@
    :wiki-url
    "/news-crawler.downloader-api.html#news-crawler.downloader/download",
    :doc
-   "Download the data in the given URL using HTTP Agents\nArgs:\n  filename - The file name to save the data in\n  url - The URL to fetch\n  milliseconds - Pause before download",
+   "Returns the web page as a string\nArgs:\n  url - The URL to fetch\n  milliseconds - Pause before download",
    :var-type "function",
    :line 16,
    :file
-   "/home/ogrim/kode/temp/egsiona/src/news_crawler/downloader.clj"}
-  {:arglists ([url-data n-streams path]),
+   "/home/ogrim/kode/news-crawler/src/news_crawler/downloader.clj"}
+  {:arglists ([urls streams]),
    :name "download-all",
    :namespace "news-crawler.downloader",
    :source-url nil,
@@ -123,11 +124,11 @@
    :wiki-url
    "/news-crawler.downloader-api.html#news-crawler.downloader/download-all",
    :doc
-   "Download data in parallel\nArgs:\n  url-data - vector of file names and URLs\n  n-streams - number of parallel downloads\n  path - folder to store files, requires trailing slash\nAdjust (rand-from-to x y) to circumvent anti-crawler scrips",
+   "Download data in parallel and returns a list of strings\nArgs:\n  url-data - vector of URLs\n  n-streams - number of parallel downloads\nAdjust (rand-from-to x y) to circumvent anti-crawler scrips",
    :var-type "function",
-   :line 30,
+   :line 28,
    :file
-   "/home/ogrim/kode/temp/egsiona/src/news_crawler/downloader.clj"}
+   "/home/ogrim/kode/news-crawler/src/news_crawler/downloader.clj"}
   {:arglists ([from to]),
    :name "rand-from-to",
    :namespace "news-crawler.downloader",
@@ -139,7 +140,7 @@
    :var-type "function",
    :line 11,
    :file
-   "/home/ogrim/kode/temp/egsiona/src/news_crawler/downloader.clj"}
+   "/home/ogrim/kode/news-crawler/src/news_crawler/downloader.clj"}
   {:arglists ([milliseconds]),
    :name "wait",
    :namespace "news-crawler.downloader",
@@ -152,7 +153,7 @@
    :var-type "function",
    :line 5,
    :file
-   "/home/ogrim/kode/temp/egsiona/src/news_crawler/downloader.clj"}
+   "/home/ogrim/kode/news-crawler/src/news_crawler/downloader.clj"}
   {:arglists ([html-map selector]),
    :name "all-links",
    :namespace "news-crawler.parser",
@@ -162,8 +163,8 @@
    "/news-crawler.parser-api.html#news-crawler.parser/all-links",
    :doc "Returns all links from the html-map by a given selector",
    :var-type "function",
-   :line 12,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"}
+   :line 17,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
   {:arglists ([name must-contain] [name must-contain & or-args]),
    :name "defilter",
    :namespace "news-crawler.parser",
@@ -174,8 +175,8 @@
    :doc
    "Defines filters functions to find articles on the main pages\nLogic:\n  must-contain AND (or-arg1 OR or-arg2 OR ..)",
    :var-type "macro",
-   :line 33,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"}
+   :line 38,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
   {:arglists ([name title body]),
    :name "defparser",
    :namespace "news-crawler.parser",
@@ -186,8 +187,8 @@
    :doc
    "Defines parser function to find title and body text from an html-map",
    :var-type "macro",
-   :line 51,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"}
+   :line 56,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
   {:arglists ([file]),
    :name "file->map",
    :namespace "news-crawler.parser",
@@ -198,7 +199,7 @@
    :doc "Reads a html file and returns a map",
    :var-type "function",
    :line 6,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"}
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
   {:arglists ([html-map title body]),
    :name "parse-article",
    :namespace "news-crawler.parser",
@@ -209,8 +210,19 @@
    :doc
    "Extracts title and body text from a html-map by using selectors",
    :var-type "function",
-   :line 42,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"}
+   :line 47,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
+  {:arglists ([string]),
+   :name "str->map",
+   :namespace "news-crawler.parser",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "/news-crawler.parser-api.html#news-crawler.parser/str->map",
+   :doc "Reads a html as string and returns a map",
+   :var-type "function",
+   :line 12,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"}
   {:arglists ([link]),
    :name "validate-link",
    :namespace "news-crawler.parser",
@@ -220,5 +232,5 @@
    "/news-crawler.parser-api.html#news-crawler.parser/validate-link",
    :doc "Simplistic link validation such as minimum length",
    :var-type "function",
-   :line 17,
-   :file "/home/ogrim/kode/temp/egsiona/src/news_crawler/parser.clj"})}
+   :line 22,
+   :file "/home/ogrim/kode/news-crawler/src/news_crawler/parser.clj"})}
